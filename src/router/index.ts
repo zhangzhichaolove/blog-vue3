e29ccc40f1ef8aa8bb131099e6985e3b48b7329f
peakchao <zhangzhichaolove@vip.qq.com>
2021-10-25 17:45:48 +0800

    update router auth.

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,8 +7,24 @@ const router = createRouter({
     history,
     routes: [
         { path: '/header', component: Header },
-        { path: '/content', component: HelloWorld }
+        { path: '/content', component: HelloWorld, meta: { isAuth: true } }
     ]
 })
+
+//之前
+router.beforeEach((to, from, next) => {
+    //验证页面权限
+    if (to.meta.isAuth && !localStorage.getItem('user')) {
+        router.replace('/')
+    } else {
+        next()
+    }
+    // console.log('beforeEach--->', to, from, next);
+})
+
+//之后
+router.afterEach(((to, from) => {
+    // console.log('afterEach--->', to, from);
+}))
 
 export default router
