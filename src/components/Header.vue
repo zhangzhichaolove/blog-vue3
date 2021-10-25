4c159e9c35f0f939b996cce08a1c33129e427d57
peakchao <zhangzhichaolove@vip.qq.com>
2021-10-25 17:17:36 +0800

    update header file.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,21 @@
 <template>
   <h1>头部</h1>
+  <h5>{{ count }}</h5>
+  <button @click="click">点击</button>
 </template>
 
-<script>
-export default {};
+<script lang='ts'>
+import { ref } from "vue";
+export default {
+  name: "Header",
+  setup(props) {
+    const count = ref(0);
+    function click() {
+      count.value++;
+    }
+    return { count, click };
+  },
+};
 </script>
 
 <style>
