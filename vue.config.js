ef41ed2d460427a0e36dd90ae0d0d7d0739fb714
peakchao <zhangzhichaolove@vip.qq.com>
2021-10-26 21:11:26 +0800

    add api proxy and license.

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,23 @@ module.exports = {
             warning: false,
             errors: false
         }
-    }
-  }
+    },
+    devServer: {
+        overlay: {
+            warning: false,
+            errors: false
+        },
+        // proxy: 'http://localhost:4000',
+        proxy: {
+            '/api': {
+                target: 'http://localhost:4000',
+                pathRewrite: { '^/api': '' },
+                ws: true,
+                changeOrigin: true
+            },
+            '/foo': {
+                target: 'http://localhost:4000'
+            }
+        }
+    },
+}
