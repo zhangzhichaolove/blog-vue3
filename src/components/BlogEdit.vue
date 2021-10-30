fe7bcd98cebc08f29480a07c6095afaf82575672
peakchao <zhangzhichaolove@vip.qq.com>
2021-10-30 16:40:48 +0800

    update admin page tabs.

--- src/components/BlogEdit.vue
+++ src/components/BlogEdit.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-md-editor v-model="text" height="100%"></v-md-editor>
+  <div class="blog-edit-container">
+    <el-input type="text" v-model="title" placeholder="文章标题" clearable />
+    <v-md-editor v-model="text" height="100%"></v-md-editor>
+  </div>
 </template>
 
 <script>
@@ -9,10 +12,16 @@ export default {
   setup(props) {
     return {
       text: ref(""),
+      title: ref(""),
     };
   },
 };
 </script>
 
 <style>
+.blog-edit-container {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+}
 </style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,8 +3,8 @@
     <el-header>Header</el-header>
     <el-container>
       <el-menu
-        default-active="1"
-        :default-openeds="['1-4-1', '2-4-1']"
+        :default-active="activeIndex"
+        :default-openeds="['2']"
         class="el-menu-vertical"
         :collapse="isCollapse"
       >
@@ -13,51 +13,19 @@
           <i v-if="!isCollapse" class="el-icon-d-arrow-left"></i>
           <template #title>{{ isCollapse ? "展开" : "收起" }}</template>
         </el-menu-item>
-        <el-sub-menu index="2">
-          <template #title>
-            <i class="el-icon-location"></i>
-            <span>菜单一</span>
-          </template>
-          <el-menu-item-group>
-            <template #title><span>组一</span></template>
-            <el-menu-item index="1-1" route="`/about`">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="组二">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title><span>选项4</span></template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-          </el-sub-menu>
-        </el-sub-menu>
-        <el-sub-menu index="3">
-          <template #title>
-            <i class="el-icon-cherry"></i>
-            <span>菜单二</span>
-          </template>
-          <el-menu-item-group>
-            <template #title><span>组一</span></template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="组二">
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="2-4">
-            <template #title><span>选项4</span></template>
-            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-          </el-sub-menu>
-        </el-sub-menu>
-        <el-menu-item index="4">
-          <i class="el-icon-menu"></i>
-          <template #title>菜单三</template>
+        <el-menu-item index="2" @click="itemClick">
+          <i class="el-icon-wind-power"></i>
+          <template #title>发布文章</template>
         </el-menu-item>
-        <el-menu-item index="5" disabled>
+        <el-menu-item index="3" @click="itemClick">
+          <i class="el-icon-menu"></i>
+          <template #title>打开版权</template>
+        </el-menu-item>
+        <el-menu-item index="4" @click="itemClick" disabled>
           <i class="el-icon-document"></i>
           <template #title>菜单四</template>
         </el-menu-item>
-        <el-menu-item index="6">
+        <el-menu-item index="5" @click="itemClick">
           <i class="el-icon-setting"></i>
           <template #title>菜单五</template>
         </el-menu-item>
@@ -65,14 +33,20 @@
       <!-- </el-aside> -->
       <el-container>
         <el-main>
-          <el-tabs type="card" v-model="editableTabsValue" closable>
+          <el-tabs
+            type="card"
+            v-model="editableTabsValue"
+            @tab-click="tatabClick"
+            @tab-remove="removeTab"
+            closable
+          >
             <el-tab-pane
-              v-for="item in editableTabs"
+              v-for="item in tabs"
               :key="item.name"
               :label="item.title"
               :name="item.name"
             >
-              <BlogEdit />
+              <component :is="item.component"></component>
             </el-tab-pane>
           </el-tabs>
         </el-main>
@@ -85,7 +59,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive, toRefs } from "vue";
 export default {
   name: "Admin",
   components: {
@@ -94,22 +68,66 @@ export default {
   },
   setup() {
     const isCollapse = ref(true);
-    const editableTabsValue = ref("t1");
 
-    let editableTabs = ref([
-      {
-        title: "标签一",
-        name: "t1",
-      },
-      {
-        title: "标签二",
-        name: "t2",
-      },
-    ]);
+    let editableTabs = reactive({
+      activeIndex: "2",
+      editableTabsValue: "publish",
+      tabs: [
+        {
+          title: "发布文章",
+          name: "publish",
+          component: <BlogEdit />,
+        },
+      ],
+    });
+    function itemClick(menu) {
+      let pushItem = undefined;
+      if (menu.index === "2") {
+        pushItem = {
+          title: "发布文章",
+          name: "publish",
+          component: <BlogEdit />,
+        };
+      } else if (menu.index === "3") {
+        pushItem = {
+          title: "打开版权",
+          name: "copyright",
+          component: <Footer />,
+        };
+      }
+      if (
+        pushItem &&
+        editableTabs.tabs.filter((item) => item.name === pushItem.name) <= 0
+      ) {
+        editableTabs.editableTabsValue = pushItem.name;
+        editableTabs.tabs.push(pushItem);
+        editableTabs.activeIndex = menu.index;
+      }
+      if (pushItem) {
+        editableTabs.editableTabsValue = pushItem.name;
+      }
+    }
+    function tatabClick(itemTab) {
+      editableTabs.activeIndex = "1";
+    }
+    function removeTab(itemTab) {
+      editableTabs.activeIndex = "1";
+      let tmpItem = editableTabs.tabs.filter((item) => item.name === itemTab);
+      let index = editableTabs.tabs.indexOf(tmpItem[0]);
+      editableTabs.tabs.splice(index, 1);
+      if (editableTabs.editableTabsValue === itemTab) {
+        index === 0 && index++;
+        if (editableTabs.tabs.length > 0) {
+          editableTabs.editableTabsValue = editableTabs.tabs[index - 1].name;
+        }
+      }
+    }
     return {
       isCollapse,
-      editableTabs,
-      editableTabsValue,
+      ...toRefs(editableTabs),
+      itemClick,
+      tatabClick,
+      removeTab,
     };
   },
 };
